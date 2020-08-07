5f372f580a8b9bf3c43947ba408e31790120ee15
Gregory Singleton <yadig69design@gmail.com>
2020-08-07 04:41:32 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -13,10 +13,10 @@ end
 def selection(num)
   if num == 1
      "YUM YUM MUNCH MUNCH MUNCH"
-     binding.pry
+    # binding.pry
   elsif num == 2
      "HAM HAM HAM IN MY TUMMY"
-      binding.pry
+      # binding.pry
   end
 end
 
